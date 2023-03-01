091a504881cd9096b2214ab702cf0b7eab699f0c
nekorytaylor666 <nekorytaylor2@gmail.com>
2023-03-01 23:10:00 +0800

    asdfasdf

--- src/routes.tsx
+++ src/routes.tsx
@@ -309,108 +309,108 @@ const routes: IRoute[] = [
   //         },
   //       ],
   //     },
-    ],
-  },
+  //   ],
+  // },
 
-  // --- Authentication ---
-  {
-    name: 'Authentication',
-    path: '/auth',
-    icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
-    collapse: true,
-    items: [
-      // --- Sign In ---
-      {
-        name: 'Sign In',
-        path: '/sign-in',
-        collapse: true,
-        items: [
-          {
-            name: 'Default',
-            layout: '/auth',
-            path: '/sign-in/default',
-          },
-          {
-            name: 'Centered',
-            layout: '/auth',
-            path: '/sign-in/centered',
-          },
-        ],
-      },
-      // --- Sign Up ---
-      {
-        name: 'Sign Up',
-        path: '/sign-up',
-        collapse: true,
-        items: [
-          {
-            name: 'Default',
-            layout: '/auth',
-            path: '/sign-up/default',
-          },
-          {
-            name: 'Centered',
-            layout: '/auth',
-            path: '/sign-up/centered',
-          },
-        ],
-      },
-      // --- Verification ---
-      {
-        name: 'Verification',
-        path: '/verification',
-        collapse: true,
-        items: [
-          {
-            name: 'Default',
-            layout: '/auth',
-            path: '/verification/default',
-          },
-          {
-            name: 'Centered',
-            layout: '/auth',
-            path: '/verification/centered',
-          },
-        ],
-      },
-      // --- Lock ---
-      {
-        name: 'Lock',
-        path: '/lock',
-        collapse: true,
-        items: [
-          {
-            name: 'Default',
-            layout: '/auth',
-            path: '/lock/default',
-          },
-          {
-            name: 'Centered',
-            layout: '/auth',
-            path: '/lock/centered',
-          },
-        ],
-      },
-      // --- Forgot Password ---
-      {
-        name: 'Forgot Password',
-        path: '/forgot-password',
-        collapse: true,
-        items: [
-          {
-            name: 'Default',
-            layout: '/auth',
-            path: '/forgot-password/default',
-          },
-          {
-            name: 'Centered',
-            layout: '/auth',
-            path: '/forgot-password/centered',
-          },
-        ],
-      },
-    ],
-  },
+  // // --- Authentication ---
+  // {
+  //   name: 'Authentication',
+  //   path: '/auth',
+  //   icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
+  //   collapse: true,
+  //   items: [
+  //     // --- Sign In ---
+  //     {
+  //       name: 'Sign In',
+  //       path: '/sign-in',
+  //       collapse: true,
+  //       items: [
+  //         {
+  //           name: 'Default',
+  //           layout: '/auth',
+  //           path: '/sign-in/default',
+  //         },
+  //         {
+  //           name: 'Centered',
+  //           layout: '/auth',
+  //           path: '/sign-in/centered',
+  //         },
+  //       ],
+  //     },
+  //     // --- Sign Up ---
+  //     {
+  //       name: 'Sign Up',
+  //       path: '/sign-up',
+  //       collapse: true,
+  //       items: [
+  //         {
+  //           name: 'Default',
+  //           layout: '/auth',
+  //           path: '/sign-up/default',
+  //         },
+  //         {
+  //           name: 'Centered',
+  //           layout: '/auth',
+  //           path: '/sign-up/centered',
+  //         },
+  //       ],
+  //     },
+  //     // --- Verification ---
+  //     {
+  //       name: 'Verification',
+  //       path: '/verification',
+  //       collapse: true,
+  //       items: [
+  //         {
+  //           name: 'Default',
+  //           layout: '/auth',
+  //           path: '/verification/default',
+  //         },
+  //         {
+  //           name: 'Centered',
+  //           layout: '/auth',
+  //           path: '/verification/centered',
+  //         },
+  //       ],
+  //     },
+  //     // --- Lock ---
+  //     {
+  //       name: 'Lock',
+  //       path: '/lock',
+  //       collapse: true,
+  //       items: [
+  //         {
+  //           name: 'Default',
+  //           layout: '/auth',
+  //           path: '/lock/default',
+  //         },
+  //         {
+  //           name: 'Centered',
+  //           layout: '/auth',
+  //           path: '/lock/centered',
+  //         },
+  //       ],
+  //     },
+  //     // --- Forgot Password ---
+  //     {
+  //       name: 'Forgot Password',
+  //       path: '/forgot-password',
+  //       collapse: true,
+  //       items: [
+  //         {
+  //           name: 'Default',
+  //           layout: '/auth',
+  //           path: '/forgot-password/default',
+  //         },
+  //         {
+  //           name: 'Centered',
+  //           layout: '/auth',
+  //           path: '/forgot-password/centered',
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
 ];
 
 export default routes;
